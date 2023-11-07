803cbc172f746b8a13eeceb9afdee8862aa44dfa
kevdev-11 <wholesolution.klb@gmail.com>
2023-11-07 15:44:05 +0000

    modified copilot third commit

--- comments.js
+++ comments.js
@@ -1,7 +1,7 @@
 //create web server
 const express = require('express');
 const app = express();
-const port = 3000;
+// const port = 3000;
 
 //connect to database
 const mongoose = require('mongoose');
